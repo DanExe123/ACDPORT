75674ea41ca4cb96652d022964eb0fec2050657d
Dan Fred P. Cablas <cablascablas@gmail.com>
2026-05-14 16:52:58 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import OurTeam from "./team/Ourteam";
-import WhatWeOffer from "./team/WhatWeoffer";
+import WhatWeOffer from "./team/WhatWeOffer";
 import Navbar from "./team/Navbar";
 
 export default function App() {
